3315fd0459432baa776d8a86e23ad9dae463d2ae
Suryanka Marick <surya9088907993@gmail.com>
2024-09-10 21:04:24 +0530

    frontend componets

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+import Headers from "./components/Header"
+
+function App() {
+  return (
+    <>
+      <Headers />
+      <Button>Test Button</Button>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <nav className="w-full h-[60px] bg-gray-900 text-white p-3 flex justify-between items-center">Header</nav>
+  )
+}
